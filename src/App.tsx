468d81d4e669b946aaa02d6f540bb03b07262cd6
Vitalik <vital538@gmail.com>
2024-08-24 18:18:51 +0300

    befor go to moscow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Suspense, useState } from 'react';
+import { Route, Router, Routes } from 'react-router-dom';
+import './styles/index.scss';
 import { Counter } from './components/Counter';
-import './index.scss'
+import AboutPage from './pages/AboutPage/AboutPage';
+import MainPage from './pages/MainPage/MainPage';
+import { Link } from 'react-router-dom';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+
+export enum Theme {
+    LIDHT = "light",
+    DARK = "dark",
+}
 
 const App = () => {
+    const [theme, settTheme] = useState(Theme.LIDHT);
+
+    const toggleTheme = () => {
+        settTheme(theme === Theme.DARK ? Theme.LIDHT : Theme.DARK);
+    }
     return (
         <div className='app'>
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
+
         </div>
     );
 };
